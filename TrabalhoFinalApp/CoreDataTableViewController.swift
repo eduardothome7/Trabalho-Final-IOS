aeec4ac5329fb86003bac477c17b61ce67868528
eduardo thome <eduardocanninithome@hotmail.com>
2017-09-30 16:57:09 -0300

    setup project

---
+++ TrabalhoFinalApp/CoreDataTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataTableViewController.swift
+//  TrabalhoFinalApp
+//
+//  Created by iossenac on 30/09/17.
+//  Copyright © 2017 senac. All rights reserved.
+//
+
+import Foundation
---
+++ TrabalhoFinalApp/ListRoomsController.swift
@@ -0,0 +1,9 @@
+//
+//  ListRoomsController.swift
+//  TrabalhoFinalApp
+//
+//  Created by iossenac on 30/09/17.
+//  Copyright © 2017 senac. All rights reserved.
+//
+
+import Foundation
